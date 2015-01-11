02c8dcf75855d68af40030249f15ec7da8d495ac
pete <nishizawa23@gmail.com>
2015-01-11 17:38:03 +0800

    add JNI log and JNI_Onload function.

--- hello-jni/jni/Android.mk
+++ hello-jni/jni/Android.mk
@@ -17,6 +17,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := hello-jni
-LOCAL_SRC_FILES := hello-jni.c
+LOCAL_SRC_FILES := hello-jni.c \
+	my-jni.c
+LOCAL_LDLIBS    := -lm -llog -landroid
 
 include $(BUILD_SHARED_LIBRARY)
--- hello-jni/jni/my-jni.c
+++ hello-jni/jni/my-jni.c
@@ -1,5 +1,15 @@
 #include <string.h>
 #include <jni.h>
+#include <android/log.h>
+#include <assert.h>
+
+#define  LOG_TAG    "myjni"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+
+/* Set to 1 to enable debug log traces. */
+#define DEBUG 1
 
 /* This is a trivial JNI example where we use a native method
  * to return a new VM String. See the corresponding Java source
@@ -7,10 +17,27 @@
  *
  *   apps/samples/hello-jni/project/src/com/example/hellojni/HelloJni.java
  */
-jstring
-Java_com_example_hellojni_HelloJni_exampleFromJNI( JNIEnv* env,
-                                                  jobject thiz )
-{
+jstring Java_com_example_hellojni_HelloJni_exampleFromJNI(JNIEnv* env,
+		jobject thiz) {
 
-    return (*env)->NewStringUTF(env, "Example Hello from JNI !");
+	return (*env)->NewStringUTF(env, "Example Hello from JNI !");
+}
+
+jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+
+	LOGI("JNI_OnLoad");
+
+	JNIEnv* env = NULL;
+	jint result = -1;
+
+	if ((*vm)->GetEnv(vm, (void**) &env, JNI_VERSION_1_4) != JNI_OK) {
+		LOGE("load library error 1");
+		return JNI_ERR;
+	}
+
+	assert(env != NULL);
+
+	result = JNI_VERSION_1_4;
+	LOGI("load library success: %d", result);
+	return result;
 }
