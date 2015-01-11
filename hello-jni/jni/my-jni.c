1abdad17850f1e1c9d52c705784ae43b237530bf
pete <nishizawa23@gmail.com>
2015-01-11 19:51:06 +0800

    add RegisterNativeMethods on JNI_Onload and build for x86

--- hello-jni/jni/my-jni.c
+++ hello-jni/jni/my-jni.c
@@ -11,6 +11,8 @@
 /* Set to 1 to enable debug log traces. */
 #define DEBUG 1
 
+static const char* kClassName="com/example/hellojni/HelloJni";
+
 /* This is a trivial JNI example where we use a native method
  * to return a new VM String. See the corresponding Java source
  * file located at:
@@ -21,6 +23,35 @@ jstring Java_com_example_hellojni_HelloJni_exampleFromJNI(JNIEnv* env,
 		jobject thiz) {
 
 	return (*env)->NewStringUTF(env, "Example Hello from JNI !");
+}
+
+static JNINativeMethod gMethods[] = { { "exampleFromJNI",
+		"()Ljava/lang/String;",
+		(void*) Java_com_example_hellojni_HelloJni_exampleFromJNI }, };
+
+static int RegisterNativeMethods(JNIEnv* env, const char* className,
+		JNINativeMethod* gMethods, int numMethods) {
+
+	LOGI("RegisterNativeMethods");
+
+	jclass clazz;
+	//in C++ use env->FindClass(className);
+	clazz = (*env)->FindClass(env, className);
+
+	//in C++ use env->RegisterNatives(clazz, gMethods, numMethods);
+	if ((*env)->RegisterNatives(env, clazz, gMethods, numMethods) < 0) {
+		return JNI_FALSE;
+	}
+
+	return JNI_TRUE;
+}
+
+static int registerNatives(JNIEnv *env) {
+	if (!RegisterNativeMethods(env, kClassName, gMethods,
+			sizeof(gMethods) / sizeof(gMethods[0]))) {
+		return JNI_FALSE;
+	}
+	return JNI_TRUE;
 }
 
 jint JNI_OnLoad(JavaVM* vm, void* reserved) {
@@ -37,7 +68,14 @@ jint JNI_OnLoad(JavaVM* vm, void* reserved) {
 
 	assert(env != NULL);
 
+	if (registerNatives(env) != JNI_TRUE) {
+		LOGE("ERROR: hello jni native registration failed\n");
+		goto fail;
+	}
+
 	result = JNI_VERSION_1_4;
 	LOGI("load library success: %d", result);
 	return result;
+
+	fail: return -1;
 }
